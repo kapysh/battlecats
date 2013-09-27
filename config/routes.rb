c2a28a98ffa7b32559c5e9a6295b992a6b9fcea9
Will Speilberg <will@kapysh.com>
2013-09-26 23:41:58 -0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Battlecats::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
