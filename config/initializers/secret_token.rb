e2fa9b43c4494ed4d989d7a40646d882707caa09
Will Speilberg <will@kapysh.com>
2013-09-26 22:37:24 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Battlecats::Application.config.secret_key_base = '3d067c6c213916beacaacb041b00bb11faa1e08cf7c40e91c1c67d1286098a46b9e11e0a2f8d0cf07d859ec641ec71c8c77ff59279f4866f2dcdc0c24f755fba'
