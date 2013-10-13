5fe3451b868ea1a5532d75073e80c4e94a2c5b66
Will Speilberg <will@kapysh.com>
2013-10-13 10:32:43 -0700

    pushing layouts and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,44 +1,56 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
   describe "Home page" do
+    before {visit home_path}
+
     it "should have the content 'battlecats'" do
-      visit '/static_pages/home'
       expect(page).to have_content('battlecats')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
       expect(page).to have_title("battlecats")
     end
 
     it "should not have the custom page title" do
-      visit '/static_pages/home'
       expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do
+    before {visit help_path}
+
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
       expect(page).to have_title("battlecats | Help")
     end
   end
 
   describe "About page" do
+    before {visit about_path}
+
     it "should have the content 'About'" do
-      visit '/static_pages/about'
       expect(page).to have_content('About')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
       expect(page).to have_title("battlecats | About")
+    end
+  end
+
+  describe "Contact" do
+    before {visit contact_path}
+
+    it "should have the content 'Contact'" do
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      expect(page).to have_title("battlecats | Contact")
     end
   end
 end
